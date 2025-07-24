3534259c55af9948995e0feeaf0743cbb8d1b632
femboyfireball <111195856+femboyfireball@users.noreply.github.com>
2025-07-23 21:36:22 -0500

    Add constants, import constants, create .gitignore

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
+from constants import SCREEN_WIDTH, SCREEN_HEIGHT, ASTEROID_MIN_RADIUS, ASTEROID_KINDS, ASTEROID_SPAWN_RATE, ASTEROID_MAX_RADIUS
+
 def main():
     print("Starting Asteroids!")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
 
 
 if __name__ == "__main__":
