be41a0e3637414105e7e48a09f441054652c4413
femboyfireball <111195856+femboyfireball@users.noreply.github.com>
2025-07-24 16:46:33 -0500

    Render a black screen, maybe handle close events

--- main.py
+++ main.py
@@ -1,9 +1,29 @@
+import pygame
+
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT, ASTEROID_MIN_RADIUS, ASTEROID_KINDS, ASTEROID_SPAWN_RATE, ASTEROID_MAX_RADIUS
 
+
 def main():
+    pygame_module_status = pygame.init()
+    signal.signal(signal.SIGQUIT, handle_sigquit)
+    print(f"Successfully loaded {pygame_module_status[0]} pygame modules. {pygame_module_status[1]} modules failed to load.")
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
+
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    main_loop(screen)
+
+def main_loop(screen):
+    color = 0x000000
+    while True:
+        # Should work??? Doesn't respond to SIGQUIT so I can't test :shrug:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.QUIT()
+
+        screen.fill("#000000")
+        pygame.display.flip()
 
 
 if __name__ == "__main__":
